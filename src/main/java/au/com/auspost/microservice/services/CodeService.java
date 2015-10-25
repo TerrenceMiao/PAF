c2ba754fdb80a6a09ce9e2755b68935895993473
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2015-10-26 01:48:19 +1100

    Add CodeService and unit test

---
+++ src/main/java/au/com/auspost/microservice/services/CodeService.java
@@ -0,0 +1,30 @@
+package au.com.auspost.microservice.services;
+
+import au.com.auspost.microservice.domain.Code;
+import au.com.auspost.microservice.repositories.CodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by terrence on 26/10/15.
+ */
+public interface CodeService {
+
+    List<Code> getCodeListByTypeItem(String typeItem);
+
+    @Service
+    class Impl implements CodeService {
+
+        @Autowired
+        CodeRepository codeRepository;
+
+        @Override
+        public List<Code> getCodeListByTypeItem(String typeItem) {
+
+            return codeRepository.findByTypeItem(typeItem);
+        }
+    }
+
+}
---
+++ src/test/java/au/com/auspost/microservice/services/CodeServiceTest.java
@@ -0,0 +1,33 @@
+package au.com.auspost.microservice.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by terrence on 26/10/15.
+ */
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CodeServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCodeListByTypeItem() throws Exception {
+
+    }
+}
