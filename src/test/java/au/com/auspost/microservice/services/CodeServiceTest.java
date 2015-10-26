067cdc5c7f5c72ef7bb576618efb3aa299c8446f
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2015-10-26 22:43:54 +1100

    Add a failed Code Service test

--- src/test/java/au/com/auspost/microservice/services/CodeServiceTest.java
+++ src/test/java/au/com/auspost/microservice/services/CodeServiceTest.java
@@ -1,12 +1,30 @@
 package au.com.auspost.microservice.services;
 
+import au.com.auspost.microservice.configuration.RepositoryConfiguration;
+import au.com.auspost.microservice.domain.Code;
+import au.com.auspost.microservice.repositories.CodeRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by terrence on 26/10/15.
  */
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {RepositoryConfiguration.class})
 public class CodeServiceTest {
+
+    private CodeService codeService = new CodeService.Impl();
 
     @Before
     public void setUp() throws Exception {
@@ -16,6 +34,19 @@ public class CodeServiceTest {
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void testGetCodeListByTypeItem() throws Exception {
+
+        List<Code> codeList = codeService.getCodeListByTypeItem("FLOOR");
+
+        assertEquals("Wrong list size", 1, codeList.size());
+
+        assertEquals("Wrong Code Type Id", "FLT", codeList.get(0).getTypeId());
+        assertEquals("Wrong Code Type Item", "FLOOR", codeList.get(0).getTypeItem());
+        assertEquals("Wrong Code Type Item Abbr", "FL", codeList.get(0).getTypeItemAbbr());
+        assertEquals("Wrong Code Type Action Code", "V", codeList.get(0).getTypeActnCode());
     }
 
 }
