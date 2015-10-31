608171ca4b70ecda74241b34784eba60c874a1ed
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2015-10-31 14:05:06 +1100

    No wildcard import

--- src/main/java/au/com/auspost/microservice/domain/DeliveryPoint.java
+++ src/main/java/au/com/auspost/microservice/domain/DeliveryPoint.java
@@ -1,7 +1,10 @@
 package au.com.auspost.microservice.domain;
 
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
 
 /**
  * Created by terrence on 28/10/15.
