48e730a678554d1cbb400d5f016ea96e8faab50b
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-01-03 11:19:29 +1100

    Remove un-necessary extension of SpringBootServletInitializer class in App

--- src/main/java/org/paradise/microservice/App.java
+++ src/main/java/org/paradise/microservice/App.java
@@ -2,18 +2,10 @@ package org.paradise.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-
-        return builder.sources(App.class);
-    }
+public class App {
 
     @Bean
     JavaScriptEngine nashornEngine() {
