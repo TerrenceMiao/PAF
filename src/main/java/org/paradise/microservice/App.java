9d37445a2ccce96852014df5e5074a7472b1b036
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-01-16 22:43:42 +1100

    Initiate Hawtio solution

--- src/main/java/org/paradise/microservice/App.java
+++ src/main/java/org/paradise/microservice/App.java
@@ -1,11 +1,26 @@
 package org.paradise.microservice;
 
+import io.hawt.config.ConfigFacade;
+import io.hawt.springboot.HawtPlugin;
+import io.hawt.springboot.PluginService;
+import io.hawt.system.ConfigManager;
+import io.hawt.web.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.hawt.springboot.EnableHawtio;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
 @SpringBootApplication
+@EnableHawtio
 public class App {
+
+    @Autowired
+    private ServletContext servletContext;
 
     @Bean
     JavaScriptEngine nashornEngine() {
@@ -17,7 +32,60 @@ public class App {
 
     public static void main(String[] args) {
 
+        System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+
         SpringApplication.run(App.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+
+        final ConfigManager configManager = new ConfigManager();
+
+        configManager.init();
+        servletContext.setAttribute("ConfigManager", configManager);
+    }
+
+    /**
+     * Loading an example plugin.
+     *
+     * @return HawtPlugin
+     */
+    @Bean
+    public HawtPlugin samplePlugin() {
+
+        return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[] { "sample-plugin/js/sample-plugin.js" });
+    }
+
+    /**
+     * Set things up to be in offline mode.
+     *
+     * @return ConfigFacade
+     * @throws Exception bean init exception
+     */
+    @Bean
+    public ConfigFacade configFacade() throws Exception {
+
+        ConfigFacade config = new ConfigFacade() {
+            public boolean isOffline() {
+                return true;
+            }
+        };
+
+        config.init();
+
+        return config;
+    }
+
+    /**
+     * Register rest endpoint to handle requests for /plugin, and return all registered plugins.
+     *
+     * @return PluginService
+     */
+    @Bean
+    public PluginService pluginService() {
+
+        return new PluginService();
     }
 
 }
