d6256d22d24e6f051d7cc2a919ea26c2d0201e57
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-07-31 03:16:14 +1000

    Add more comments in create_indexes.sql

--- db/mysql/create_indexes.sql
+++ db/mysql/create_indexes.sql
@@ -1,20 +1,25 @@
 use paf;
 
+# LOCALITY
 CREATE UNIQUE INDEX locality_id_index on locality(locality_id) USING HASH;
 CREATE INDEX locality_name_index on locality(locality_name) USING HASH;
 CREATE INDEX postcode_index on locality(postcode) USING HASH;
 CREATE INDEX state_index on locality(state) USING HASH;
 
+# DELIVERY_POINT_GROUP
 CREATE UNIQUE INDEX delivy_point_group_id_index on delivery_point_group(delivy_point_group_id) USING HASH;
 CREATE INDEX locality_id_index on delivery_point_group(locality_id) USING HASH;
 CREATE INDEX street_name_index on delivery_point_group(street_name) USING HASH;
 CREATE INDEX street_type_index on delivery_point_group(street_type) USING HASH;
 CREATE INDEX postal_delivery_type_index on delivery_point_group(postal_delivery_type) USING HASH;
 
+# SYNONYM
 CREATE INDEX locality_id_index on synonym(locality_id) USING HASH;
 
+# BORDERING_LOCALITY
 CREATE INDEX parent_locality_id_index on bordering_locality(parent_locality_id) USING HASH;
 
+# DELIVERY_POINT
 CREATE UNIQUE INDEX delivy_point_id_index on delivery_point(delivy_point_id) USING HASH;
 CREATE INDEX delivy_point_group_id_index on delivery_point(delivy_point_group_id) USING HASH;
 CREATE INDEX house_nbr_1_index on delivery_point(house_nbr_1) USING HASH;
