5626b35dcef6fd129e82f60c3ad2b57e55d20e0c
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-08-14 20:21:31 +1000

    Add POL (Post Office Location) database schema and Quartz schema

--- db/mysql/create_database.sql
+++ db/mysql/create_database.sql
@@ -1,1 +1,3 @@
 create database paf;
+
+CREATE DATABASE pol;
--- db/mysql/create_user.sql
+++ db/mysql/create_user.sql
@@ -1,7 +1,16 @@
 ## localhost access ONLY
-# create user 'paf'@'localhost' identified by 'password';
-# grant all privileges on *.* to 'paf'@'localhost' with grant option;
+
+## PAF (Postal Address File)
+create user 'paf'@'localhost' identified by 'password';
+grant all privileges on *.* to 'paf'@'localhost' with grant option;
+
+## POL (Post Office Location)
+create user 'pol'@'localhost' identified by 'password';
+grant all privileges on *.* to 'pol'@'localhost' with grant option;
 
 ## Global access
 create user 'paf'@'%' identified by 'password';
 grant all privileges on *.* to 'paf'@'%' with grant option;
+
+create user 'pol'@'%' identified by 'password';
+grant all privileges on *.* to 'pol'@'%' with grant option;
