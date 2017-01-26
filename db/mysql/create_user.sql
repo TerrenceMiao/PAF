508b7e37ad60d5f17474c5bf0bef4f029791bd70
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-01-27 10:24:27 +1100

    Update create_user.sql with restricted and limited access for DB user to database

--- db/mysql/create_user.sql
+++ db/mysql/create_user.sql
@@ -2,15 +2,15 @@
 
 ## PAF (Postal Address File)
 create user 'paf'@'localhost' identified by 'password';
-grant all privileges on *.* to 'paf'@'localhost' with grant option;
+grant all privileges on paf.* to 'paf'@'localhost' with grant option;
 
 ## POL (Post Office Location)
 create user 'pol'@'localhost' identified by 'password';
-grant all privileges on *.* to 'pol'@'localhost' with grant option;
+grant all privileges on pol.* to 'pol'@'localhost' with grant option;
 
 ## Global access
 create user 'paf'@'%' identified by 'password';
-grant all privileges on *.* to 'paf'@'%' with grant option;
+grant all privileges on paf.* to 'paf'@'%' with grant option;
 
 create user 'pol'@'%' identified by 'password';
-grant all privileges on *.* to 'pol'@'%' with grant option;
+grant all privileges on pol.* to 'pol'@'%' with grant option;
